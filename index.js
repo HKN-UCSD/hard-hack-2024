e8edb99ec129cb5dee5caffba604b1012c850949
Adneths <bill.lui0921@gmail.com>
2023-02-10 16:55:11 -0800

    Add image animation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	var height = $(window).height();
 	var width = $(window).width();
-	if(width<height)
-	{
-		$("#img_fallenstar").css("top", "55vh");
-		$("#img_library").css("top", "5vh");
-	}
+	var t = Math.max(Math.min(($(window).width() - $(window).height() + 50)/250,1),0);
+	$("#img_fallenstar").css("top", (55-t*25)+"vh");
+	$("#img_library").css("top", (5+t*20)+"vh");
+	
+	$("#img_fallenstar").animate({left: "-65px"}, 1000);
+	$("#img_library").animate({right: "-50px"}, 1000);
 })
 $(window).resize(function() {
 	var height = $(window).height();
