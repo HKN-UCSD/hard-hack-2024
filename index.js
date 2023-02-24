02c123abb6b3bd3b234bb4c7e8a2a99d192abc55
Adneths <79169395+Adneths@users.noreply.github.com>
2023-02-24 10:51:24 -0800

    Revert "Bill/feature/faq section"

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 $(document).ready(function() {
 	onResize();	
 	
-	$("#img_fallenstar").animate({"--progress": "1"}, {
-		duration: 1000, step: function(val) {
-			$(this).css({"--progress": val});
-			onResize();
-		}
-	});
-	console.log();
-	$("#img_library").animate({left: Math.max(80,parseInt($("#img_library").css("left"))*7/30)+"px"}, 800, "swing", function() {
-		$(this).css({left: "max(80px,35%)"});
-	});
-});
+	$("#img_fallenstar").animate({left: "-65px"}, 1000);
+	$("#img_library").animate({right: "-50px"}, 1000);
+})
 $(window).resize(onResize);
 
 function onResize() {
 	const height = $(window).height();
 	const width = $(window).width();
-	const t = Math.max(Math.min((height - width + 250)/250,1),0);
+	const t = Math.max(Math.min((width - height + 50)/250,1),0);
+	const maxHeight = Math.max(Math.min((900 - width)/200,1),0)*430;
+	$("#img_fallenstar").css("top", "max(" + (55-t*25)+"vh," + maxHeight + "px)");
+	$("#img_library").css("top", "max(" + (5+t*20)+"vh,30px)");
 	
-	const container = $("#img_fallenstar_container");
-	const fallenstar = $("#img_fallenstar");
-	
-	$("#cover-flex").css({"padding-bottom": (48*(1+t)) + "px"});
-	
-	fallenstar.css({height: (100-30*t) + "%", top: (12*t) + "%"});
-	fallenstar.css("left", Math.min(-(1-fallenstar.css("--progress"))*fallenstar.width(),container.width()-fallenstar.width()) + "px");
+	const h = $("#cover-page").outerHeight();
+	if(h !== undefined)
+		$("#background").css("height", "max(100vh," + (h*1.1) + "px)");
 }
